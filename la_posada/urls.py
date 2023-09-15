46914df45fee638af10306398b01938c6138d090
mykki92 <mike2108@hotmail.co.uk>
2023-09-15 11:44:40 +0000

    Add views and url for menus page

--- la_posada/urls.py
+++ la_posada/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from menus.views import say_hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('menus/', say_hello, name='menus'),
 ]
