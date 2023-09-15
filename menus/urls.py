e7ced73cb520555d7c1627bb9c44083fd65f7751
mykki92 <mike2108@hotmail.co.uk>
2023-09-15 21:32:11 +0000

    Fix menu urls to access menu pages

--- menus/urls.py
+++ menus/urls.py
@@ -3,6 +3,6 @@ from menus import views
 
 # Urls for the food and drinks menu
 urlpatterns = [
-    path('', views.food_menu, name='food_menu'),
-    path('', views.drink_menu, name='drink_menu'),
+    path('food_menu', views.food_menu, name='food_menu'),
+    path('drink_menu', views.drink_menu, name='drink_menu'),
 ]
