459e9ee582aced587d26f36bb0005b221a8d63ec
mykki92 <mike2108@hotmail.co.uk>
2023-09-16 22:11:03 +0000

    Update the view for the tapas menu in menus/views.py

--- menus/views.py
+++ menus/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
+from .models import TapasItem
 
 
-# Create your views here.
-
-
+# View for the tapas menu page
 def tapas_menu(request):
-    return render(request, 'menus/tapas_menu.html')
+    tapas_list = TapasItem.objects.all()
+    return render(request, 'menus/tapas_menu.html', {'tapas_list': tapas_list})
 
 
 def drink_menu(request):
