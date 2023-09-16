6e9b4c4599f67effd7c2ba7b81b0c83398ee0cbb
mykki92 <mike2108@hotmail.co.uk>
2023-09-16 23:35:42 +0000

    Register the drink item models for the admin panel in menus/admin.py

--- menus/admin.py
+++ menus/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import TapasItem
+from .models import TapasItem, DrinkItem
 
 
 # Registration of the tapas items for the admin panel
@@ -8,4 +8,12 @@ from .models import TapasItem
 class TapasAdmin(SummernoteModelAdmin):
     list_display = ('tapas_name', 'tapas_type', 'tapas_price', 'plato_price', 'sides_price')
     search_fields = ('tapas_name', 'description')
+    summernote_fields = ('description')
+
+
+# Registration of the drinks items for the admin panel
+@admin.register(DrinkItem)
+class DrinkAdmin(SummernoteModelAdmin):
+    list_display = ('drink_name', 'drink_type', 'price')
+    search_fields = ('drink_name', 'description')
     summernote_fields = ('description')
