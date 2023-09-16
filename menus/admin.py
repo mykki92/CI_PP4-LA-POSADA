7c147986c056eae4ea8c4997813cb8df8184872c
mykki92 <mike2108@hotmail.co.uk>
2023-09-16 22:07:08 +0000

    Create TapasAdmin class in menus/admin.py

--- menus/admin.py
+++ menus/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
+from .models import TapasItem
 
-# Register your models here.
+
+# Registration of the tapas items for the admin panel
+@admin.register(TapasItem)
+class TapasAdmin(SummernoteModelAdmin):
+    list_display = ('tapas_name', 'tapas_type', 'price')
+    search_fields = ('tapas_name', 'description')
+    summernote_fields = ('description')
