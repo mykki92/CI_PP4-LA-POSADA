600e11df287be17beaee13307467eef69a4ad338
mykki92 <mike2108@hotmail.co.uk>
2023-09-16 19:09:09 +0000

    Install django summernote

--- la_posada/urls.py
+++ la_posada/urls.py
@@ -19,6 +19,7 @@ from menus.views import tapas_menu, drink_menu
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('', include('home.urls'), name='home_urls'),
     path('', include('menus.urls'), name='menus_urls'),
 ]
