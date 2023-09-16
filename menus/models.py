626256879a29e4e31d4d43be464397b806ea87e7
mykki92 <mike2108@hotmail.co.uk>
2023-09-16 23:33:18 +0000

    Create drink item model in menus/models.py and migrate to database

--- menus/models.py
+++ menus/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Food and drink categories
 TAPAS_CATEGORY = ((0, 'Salads & Cold Cuts'), (1, 'Hot Dishes'), (2, 'Sides'))
+DRINK_CATEGORY = ((0, 'Beers'), (1, 'Wines'), (2, 'Cocktails'))
+
 
 # Model for tapas menu items
-
-
 class TapasItem(models.Model):
     """
     a class for the tapas menu item model
@@ -15,7 +15,7 @@ class TapasItem(models.Model):
     description = models.CharField(max_length=200, unique=True, blank=True)
     tapas_price = models.FloatField(default=0)
     plato_price = models.FloatField(default=0)
-    sides_price = models.FloatField(default=0, blank=True)
+    sides_price = models.FloatField(default=0)
     tapas_type = models.IntegerField(choices=TAPAS_CATEGORY)
 
     class Meta:
@@ -23,3 +23,21 @@ class TapasItem(models.Model):
 
     def __str__(self):
         return self.tapas_name
+
+
+# Model for drink menu items
+class DrinkItem(models.Model):
+    """
+    a class for the drink menu item model
+    """
+    drink_id = models.AutoField(primary_key=True)
+    drink_name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=200, unique=True, blank=True)
+    price = models.FloatField()
+    drink_type = models.IntegerField(choices=DRINK_CATEGORY,)
+
+    class Meta:
+        ordering = ['-drink_type']
+
+    def __str__(self):
+        return self.drink_name
