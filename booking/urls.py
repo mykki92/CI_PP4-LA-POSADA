86b716a56e4ea8cc435794bb2a145de6389ba776
mykki92 <mike2108@hotmail.co.uk>
2023-09-17 22:13:59 +0000

    Write the url pattern for booking_confirmed page

--- booking/urls.py
+++ booking/urls.py
@@ -4,4 +4,9 @@ from booking import views
 # url for the booking page
 urlpatterns = [
     path('make_booking', views.MakeBooking.as_view(), name='make_booking'),
+    path(
+        'booking_confirmed',
+        views.BookingConfirmed.as_view(),
+        name='booking_confirmed'
+        ),
 ]
