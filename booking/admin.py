85d690c7038e6e912038cf3bb1926681e88a9c44
mykki92 <mike2108@hotmail.co.uk>
2023-09-17 21:55:21 +0000

    Create the table admin and booking admin models in admin.py

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from rangefilter.filters import DateRangeFilter
+from .models import Table, Booking
 
-# Register your models here.
+
+# Registration of the table model in the admin panel
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('table_id', 'table_number', 'seats')
+
+
+# Registration of the booking model in the admin panel
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_filter = (
+        'user',
+        'name',
+        'email',
+        'phone',
+        'party_of',
+        'status',
+        'table_id',
+        'booking_date',
+        'booking_time',
+        'created_on'
+        )
+    list_display = (
+        'booking_id',
+        'user',
+        'name',
+        'phone',
+        'party_of',
+        'status',
+        'table',
+        'requested_date',
+        'requested_time',
+        'created_date')
+
+    search_fields = ['guest__name']
+    list_filter = (('requested_date', DateRangeFilter),)
+    actions = ['confirm_bookings']
+
+    def confirm_bookings(self, request, queryset):
+        queryset.update(status='Booking Confirmed')
