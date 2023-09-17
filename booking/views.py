3b875020f6f2581788472c9e2e440bf88a9ffcab
mykki92 <mike2108@hotmail.co.uk>
2023-09-17 18:09:34 +0000

    Create view for the booking form

--- booking/views.py
+++ booking/views.py
@@ -3,8 +3,49 @@ from django.views import generic, View
 from django.contrib.auth.models import User
 from django.contrib import messages
 import datetime
+from .models import Booking
+from .forms import BookingForm
 
 
-# View to display the booking page
-def booking(request):
-    return render(request, 'booking/booking.html')
+# Retrieves user information if user is signed in
+def get_user(request):
+    user_email = request.user.email
+    user = User.objects.filter(email=user_email).first()
+    return user
+
+
+# Class to display the booking form and update the database with successful
+# bookings
+class MakeBooking(View):
+    """
+    Displays the booking form and auto fills the email field if the user
+    is signed in
+    """
+    template_name = 'booking/booking.html'
+    success_message = 'Thats in the diary, well see you soon!'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            email = request.user.email
+            booking_form = BookingForm(initial={'email': email})
+        else:
+            booking_form = BookingForm()
+        return render(request, 'booking/booking.html',
+                      {'booking_form': booking_form})
+
+    def post(self, request):
+        """
+        Checks that the provided information is valid and updates
+        the database
+        """
+        booking_form = BookingForm(data=request.POST)
+
+        if booking_form.is_valid():
+            booking = booking_form.save(commit=False)
+            booking.user = request.user
+            booking.save()
+            messages.success(
+                request, "Booking successful, awaiting confirmation")
+
+        return render(request, 'booking/booking.html',
+                      {'booking_form': booking_form})
