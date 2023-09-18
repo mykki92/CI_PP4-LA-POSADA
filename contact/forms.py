d0d0d5826873a70f7e0e4ad76fe0550b4a83e171
mykki92 <mike2108@hotmail.co.uk>
2023-09-18 16:33:38 +0000

    Create contact form in contact/forms.py

---
+++ contact/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from crispy_forms.helper import FormHelper
+from phonenumber_field.formfields import PhoneNumberField
+from .models import ContactUs
+
+
+class ContactForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+
+    phone = PhoneNumberField(widget=forms.TextInput())
+
+    class Meta:
+        model = Contact
+        fields = (
+            'name',
+            'phone',
+            'email',
+            'message'
+        )
