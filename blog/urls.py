5bc88e07d5761805f05ba1d4db0ef160901536d0
mykki92 <mike2108@hotmail.co.uk>
2023-09-21 15:38:10 +0000

    Add url patterns for the blog page to blog/urls.py

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+# url patterns for the blog page
+urlpatterns = [
+    path('', views.BlogList.as_view(), name='blog'),
+    path('<slug:slug>/', views.BlogDetail.as_view(), name='blog_detail'),
+    path('likes/<slug:slug>', views.BlogLikes.as_view(), name='blog_likes'),
+]
