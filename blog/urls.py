56e14048a05df6712af2650b9ba866495a895302
mykki92 <mike2108@hotmail.co.uk>
2023-09-21 16:28:10 +0000

    Add html to blog.html

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 # url patterns for the blog page
 urlpatterns = [
-    path('', views.BlogList.as_view(), name='blog'),
+    path('blog/', views.BlogList.as_view(), name='blog'),
     path('<slug:slug>/', views.BlogDetail.as_view(), name='blog_detail'),
     path('likes/<slug:slug>', views.BlogLikes.as_view(), name='blog_likes'),
 ]
