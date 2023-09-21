c9cbde1aaa7135c4aceeba8f584b4feebff8087d
mykki92 <mike2108@hotmail.co.uk>
2023-09-21 15:31:50 +0000

    Create view to display blog post likes

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.core.paginator import Paginator
 from .models import Post
@@ -67,3 +68,16 @@ class BlogDetail(View):
                 "liked": liked
             },
         )
+
+
+# Class to display blog post likes
+class BlogLikes(View):
+
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('', args=[slug]))
